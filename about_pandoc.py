c86ed7c96b7fe02674e0eff9c8bc8d9f54013919
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-03-22 10:43:25 +0100

    metadata bug

--- about_pandoc.py
+++ about_pandoc.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
+__project__ = "pandoc"
 __author__  = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
 __version__ = "1.0.0-alpha"
 __license__ = "MIT License"
 __url__     = "https://github.com/boisgera/pandoc"
 
-export = "author version license url".split()
+export = "project author version license url".split()
 __all__ = ["__" + name + "__" for name in export]
 
