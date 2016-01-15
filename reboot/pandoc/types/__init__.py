e26598f37a8834f88fa57ffe0ec92b5fc39fe63d
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-01-15 10:31:55 +0100

    design thoughts (records, deconstructors, etc.)

--- reboot/pandoc/types/__init__.py
+++ reboot/pandoc/types/__init__.py
@@ -1,5 +1,12 @@
 
+# Python 2.7 Standard Library
+import collections
+
+# Local Modules
 from .defs import defs
+
+# ------------------------------------------------------------------------------
+
 primitives = {"String": unicode, 
               "Bool": bool, 
               "Int": int, 
@@ -7,7 +14,7 @@ primitives = {"String": unicode,
               "tuple": tuple}
 types = {}
 singletons = {}
-typedefs = {} # name "aliases" instead
+typedefs = {} # name "aliases" instead ?
 # TODO: typedefs = {} ? What would I put in there ? A kind of spec ?
 
 # TODO: transfer to doc this paragraph.
@@ -33,15 +40,8 @@ class Type(object):
     pass
 
 class Data(Type):
-    def __init__(self, *args, **kwargs):
-        if args:
-            assert not kwargs
-            self.args = args
-            self.name = None
-        else:
-            assert "name" in kwargs
-            self.name = kwargs["name"]
-            self.args = None
+    def __init__(self, *args):
+        self.args = args
 
     def __repr__(self):
         if self.args:
@@ -49,15 +49,62 @@ class Data(Type):
             args = ", ".join(repr(arg) for arg in self.args)
             return "{0}({1})".format(typename, args)
         else:
-            return self.name
+            return self.__name__
 
     __str__ = __repr__
 
+class NewType(Type):
+    def __init__(self, *args):
+        self.args = args
+
+    def __repr__(self):
+        if self.args:
+            typename = type(self).__name__
+            args = ", ".join(repr(arg) for arg in self.args)
+            return "{0}({1})".format(typename, args)
+        else:
+            return self.__name__
+
+    __str__ = __repr__
+
+
+# Should records have all data behaviors ?
+# ----------------------------------------
+# Make a record support everything a data type does (init with args, etc.) ?
+# That would be nice somehow ... but could pose problems down the line when
+# /if we accept that those instances are mutable and we don't want to protect
+# the data access with properties to keep the internal state. Pragmatically,
+# it may be better NOT to supports args for records, only kwargs (note that
+# records are serialized differently from non-record in JSON).
+
+# Record field names
+# ------------------
+# Pragmatically, there are two schemes only that are used in Pandoc
+#
+#   - namespace protection: the Citation record prefixed all its fields with
+#     "citation" to avoid a name clashe with the generated accessors.
+#     We could maybe avoid that ... (this name clashes does not happen in Python)
+#
+#   - deconstructors: this is a feature used for records that are also newtypes: 
+#     see <https://wiki.haskell.org/Type> for an explanation. Concretely, that
+#     means that `Meta` has a single `unMeta` field (the generated function
+#     does the unwrapping).
+#
+#     So in this case actually, AFAICT, the declaration of `Meta` as a record is 
+#     merely a way to avoid the declaration of the accessor, not a true design
+#     choice about the data structure ... In Haskell this is not a big deal,
+#     because we can still use this object AS A DATA, pattern match on its
+#     arguments, etc, without worrying about the fact that it's declared as 
+#     a record ... That totally pleads in favor of having the records having 
+#     the full data behaviors, but we know that we are going to run into
+#     trouble if we allow such structures to be mutable ...
+
 class Record(Type):
-    # Shit, we are losing the order of arguments here :(
-    # Could be restored with the knowledge of the type decl however.
     def __init__(self, **kwargs):
-        self.kwargs = kwargs
+        keys = [item[0] for item in self._decl]
+        self.kwargs = collections.OrderedDict()
+        for key in keys:
+            self.kwargs[key] = kwargs.get(key)
 
     def __repr__(self):
         typename = type(self).__name__
@@ -67,7 +114,7 @@ class Record(Type):
 
     __str__ = __repr__
 
-class Typedef(Type):
+class TypeDef(Type):
     pass
 
 for decl in defs:
@@ -87,7 +134,6 @@ for decl in defs:
                 if constructor[1][0] == "record":
                     base = Record
                     decl = constructor_args[1]
-                    print "***", base, decl
                 else:
                     base = Data
                     decl = constructor_args
@@ -101,9 +147,9 @@ for decl in defs:
                     types[constructor_name] = \
                       type(constructor_name, (base,), {"_decl": constructor_args})
                 else:
-                    singletons[constructor_name] = base(name=constructor_name)
+                    singletons[constructor_name] = base(__name__=constructor_name)
     if decl_type == "type":
-        typedefs[type_name] = type(type_name, (Typedef,), {"_decl": decl[1][1]})
+        typedefs[type_name] = type(type_name, (TypeDef,), {"_decl": decl[1][1]})
         
 globals().update(primitives)
 globals().update(types)
