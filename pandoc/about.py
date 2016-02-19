74d6ec1caae58fff304cf8e2fb1f4aaaac797311
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-02-19 09:37:42 +0100

    Python standard version scheme

--- pandoc/about.py
+++ pandoc/about.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__     = "pandoc",
-  __version__  = "1.0.0-alpha.13", 
+  __version__  = "1.0.0a14", 
   __license__  = "MIT License",
   __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__      = "https://github.com/boisgera/pandoc",
