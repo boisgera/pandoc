25916b784eeba185fc1db0253d141ac4c37ac979
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-03-18 00:00:09 +0100

    metadata: add python 2.7 flag

--- pandoc/about.py
+++ pandoc/about.py
@@ -2,12 +2,12 @@
 
 metadata = dict(
   __name__     = "pandoc",
-  __version__  = "1.0.0a16", 
+  __version__  = "1.0.0a17", 
   __license__  = "MIT License",
   __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__      = "https://github.com/boisgera/pandoc",
   __summary__  = "Pandoc Documents for Python",
-  __keywords__ = "software development, topic/text processing"
+  __keywords__ = "software development, topic/text processing, python/2.7"
 )
 
 globals().update(metadata)
