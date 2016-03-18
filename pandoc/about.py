15d5ef2b386c461ecc156b5543f10a27bbc425c9
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-03-18 09:48:39 +0100

    python version and status badges

--- pandoc/about.py
+++ pandoc/about.py
@@ -2,12 +2,16 @@
 
 metadata = dict(
   __name__     = "pandoc",
-  __version__  = "1.0.0a17", 
+  __version__  = "1.0.0a18", 
   __license__  = "MIT License",
   __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__      = "https://github.com/boisgera/pandoc",
   __summary__  = "Pandoc Documents for Python",
-  __keywords__ = "software development, topic/text processing, python/2.7"
+  __keywords__ = """
+                 software development, topic/text processing, 
+                 python 2 / only, python / 2.7,
+                 alpha
+                 """
 )
 
 globals().update(metadata)
