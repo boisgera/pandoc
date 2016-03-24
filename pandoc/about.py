5f78ea0ad15c4a9498f45f233070d6b2eb45f1c2
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-03-24 18:36:02 +0100

    updated metadata for python 3

--- pandoc/about.py
+++ pandoc/about.py
@@ -2,14 +2,14 @@
 
 metadata = dict(
   __name__     = "pandoc",
-  __version__  = "1.0.0b1", 
+  __version__  = "1.0.0b2", 
   __license__  = "MIT License",
   __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__      = "https://github.com/boisgera/pandoc",
   __summary__  = "Pandoc Documents for Python",
   __keywords__ = """
                  software development, topic/text processing, 
-                 python 2 / only, python / 2.7,
+                 python / 2, python / 3,
                  beta
                  """
 )
