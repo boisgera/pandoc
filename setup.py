90260ff40e58237b981ac7c0cd1be4fdf91f6519
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2019-01-29 14:36:07 +0100

    simplify setup.py (about dep.)

--- setup.py
+++ setup.py
@@ -19,38 +19,16 @@ except ImportError:
     error = "pip is not installed, refer to <{url}> for instructions."
     raise ImportError(error.format(url="http://pip.readthedocs.org"))
 
-def local(path):
-    return os.path.join(os.path.dirname(__file__), path)
-
-# Extra Third-Party Libraries
+# Local Libraries
 # ------------------------------------------------------------------------------
-sys.path.insert(0, local(".lib"))
-setup_requires = ["about>=5.1,<6"]
-def not_found(req):
-    error  = "{req!r} not found; install it locally with:\n"
-    error += "    pip install --target=.lib --ignore-installed {req!r}"
-    return ImportError(error.format(req=req))
-def local_conflict(req, found):
-    error  = "Found {found!r} locally that conflicts with {req!r}.\n"
-    error += "Delete the '.lib' directory and start over."
-    raise ImportError(error.format(found=found, req=req))
-for req in setup_requires:
-    try:
-        require = lambda *r: pkg_resources.WorkingSet().require(*r)
-        require(req)
-    except pkg_resources.DistributionNotFound:
-        raise not_found(req)
-    except pkg_resources.VersionConflict as version_error:
-        found_dist = version_error.args[0]
-        found = found_dist.project_name + "==" + found_dist.version
-        if found_dist.location == local(".lib"):
-            raise local_conflict(req=req, found=found)
-        else:
-            raise not_found(req)
-import about
+
 
 # Pandoc Metadata
 # ------------------------------------------------------------------------------
+def local(path):
+    return os.path.join(os.path.dirname(__file__), path)
+sys.path.insert(0, local(".lib"))
+import about
 tmp_dir = tempfile.mkdtemp()
 source = local("src/pandoc/about.py")
 target = os.path.join(tmp_dir, "about_pandoc.py")
@@ -59,10 +37,11 @@ sys.path.insert(0, tmp_dir)
 import about_pandoc
 del sys.path[0]
 shutil.rmtree(tmp_dir)
+metadata = about.get_metadata(about_pandoc)
 
 # Setup Configuration
 # ------------------------------------------------------------------------------
-metadata = about.get_metadata(about_pandoc)
+
 contents = {
   "packages": setuptools.find_packages("src"),
   "package_dir": {"": "src"},
