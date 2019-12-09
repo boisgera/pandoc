4040114aaf136fe7d530e6e1584be647463a6e37
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2019-12-09 22:16:54 +0100

    pandoc-types fetch script

--- fetch-types/main.py
+++ fetch-types/main.py
@@ -2,7 +2,7 @@
 
 # Python 2.7 Standard Library
 import json
-import StringIO
+import io
 import re
 import sys
 
@@ -49,24 +49,24 @@ def write_version_info(output):
     versions = []
     for string in strings:
         if len(string) >= 1 and string[0] in '0123456789':
-            versions.append(string.encode('utf-8'))
+            versions.append(string)
     
     # 1.8 is good enough (JSON support starts there)
     versions = [v for v in versions if version_key(v) >= [1, 8]]
 
     output.write('{\n')
     for i, version in enumerate(versions):
-        print version + ":",
+        print(version + ":", end="")
         output.write('"' + str(version) + '": ') 
         url = 'http://hackage.haskell.org/package/pandoc-{0}/src/pandoc.cabal'
         url = url.format(version)
-        cabal_file = requests.get(url).content
+        cabal_file = requests.get(url).content.decode("utf-8")
         for line in cabal_file.splitlines():
             line = line.strip()
             if 'pandoc-types' in line:
                 vdep = line[13:-1]
                 vdep = [c.strip().split(' ') for c in vdep.split('&&')]
-                print vdep
+                print(vdep)
                 output.write(json.dumps(vdep))
                 if i < len(versions) - 1:
                     output.write(',')
@@ -74,7 +74,7 @@ def write_version_info(output):
                 break
     output.write('}')
 
-tmp = StringIO.StringIO()
+tmp = io.StringIO()
 write_version_info(tmp)
 version_map = json.loads(tmp.getvalue())
 
@@ -109,7 +109,7 @@ def write_type_definitions(output):
     sh.cd("pandoc-types")
 
     # get the version tags, write the (ordered) list down
-    versions = sh.git("tag").split()
+    versions = str(sh.git("tag")).split()
     versions.sort(key=version_key)
 
     # 1.8 is good enough (JSON support starts there)
@@ -142,17 +142,17 @@ def write_type_definitions(output):
             stack_not_found = False
         sh.git("checkout", ".")
 
-        print 80*"-"
+        print(80*"-")
 
         sh.git("checkout", version)
 
         try:
             stack_not_found = False
             sh.ls("stack.yaml")
-            print "{0}: found stack.yaml file".format(version)
+            print("{0}: found stack.yaml file".format(version))
         except:
             stack_not_found = True
-            print "{0}: no stack.yaml file found".format(version)
+            print("{0}: no stack.yaml file found".format(version))
             sh.stack("init", "--solver")
     #        stack_file = open("stack.yaml", "w")
     #        stack_file.write(STACK_YAML)
@@ -184,26 +184,28 @@ def write_type_definitions(output):
         b.close()
 
         try:
-            print "setting up stack ..."
+            print("setting up stack ...")
             for line in sh.stack("setup", _iter=True, _err_to_out=True):
-                print "   ", line,
-            print "building ..."
+                print("   ", line, end="")
+            print("building ...")
             for line in sh.stack("build", _iter=True, _err_to_out=True):
-                print "   ", line,
+                print("   ", line, end="")
         except Exception as error:
             message = error.stdout
             for line in message.splitlines():
-                print >> sys.stderr, "    ", line
+                print("    ", line, file=sys.stderr)
             # TODO: print error to file (.error instead of .hs)
             # this filter here is not good enough
-            ansi_escape = re.compile(r'\x1b[^m]*m')
-            message = ansi_escape.sub('', message)
+
+            ansi_escape = re.compile(rb'\x1b[^m]*m')
+            message = ansi_escape.sub(b'', message)
+
             #message = "".join(char for char in message if isprint(char))
             #open("../definitions/{0}.error".format(version), "w").write(message)
             #continue
-            raise RuntimeError(message)
+            raise RuntimeError(message.decode("utf-8"))
 
-        print "running GHCI script ..."
+        print("running GHCI script ...")
         collect = False
         lines = []
         for line in sh.stack("ghci", _iter=True):
@@ -231,11 +233,14 @@ def write_type_definitions(output):
 
 # Main
 # ------------------------------------------------------------------------------
+
 if __name__ == '__main__':
     output = open('pandoc-types.js', 'w')
     output.write('{\n')
+    # temporary outcomment
     output.write('"version_mapping":\n')
-    write_version_info(output)
+    json.dump(version_map, output)
+    #write_version_info(output)
     output.write(',\n')
     output.write('"definitions":\n') 
     write_type_definitions(output)
