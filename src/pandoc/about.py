d9904bbf35c28c75b8334d2cfbd1798a9f397ca3
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-03-28 14:39:05 +0100

    update metadata

--- src/pandoc/about.py
+++ src/pandoc/about.py
@@ -2,9 +2,9 @@
 
 metadata = dict(
   __name__     = "pandoc",
-  __version__  = "1.1.0", 
+  __version__  = "2.0-a1", 
   __license__  = "MIT License",
-  __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
+  __author__   = "Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__      = "https://github.com/boisgera/pandoc",
   __summary__  = "Pandoc Documents for Python",
   __keywords__ = """
