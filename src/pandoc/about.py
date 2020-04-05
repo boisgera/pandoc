a4d96ea70cb3fb64bffe07e2f7f0d2bf202f5774
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-04-05 12:44:27 +0200

    add python 3 metadata

--- src/pandoc/about.py
+++ src/pandoc/about.py
@@ -7,6 +7,7 @@ description = "Pandoc Documents for Python"
 author = "Sébastien Boisgérault"
 author_email = "Sebastien.Boisgerault@mines-paristech.fr"
 classifiers = [
+  "Programming Language :: Python :: 3",
   "Development Status :: 3 - Alpha",
   "Topic :: Software Development",
   "Topic :: Text Editors :: Text Processing"
