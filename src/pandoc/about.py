a8477487588e78d33f268aff3b81b5e8577218e6
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-12-04 13:09:38 +0100

    remove macos ci, update metadata

--- src/pandoc/about.py
+++ src/pandoc/about.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 
 name = "pandoc"
-version = "2.0.0"
+version = "2.0.1"
 license = "MIT License"
 description = "Pandoc Documents for Python"
 author = "Sébastien Boisgérault"
 author_email = "Sebastien.Boisgerault@mines-paristech.fr"
 classifiers = [
     "Programming Language :: Python :: 3",
-    "Development Status :: 3 - Alpha",
+    "Development Status :: 5 - Production/Stable",
     "Topic :: Software Development",
     "Topic :: Text Editors :: Text Processing",
 ]
