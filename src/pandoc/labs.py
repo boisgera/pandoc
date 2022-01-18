1cbb755a3aabd73a0fb76889f05033e657fa6a3f
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-01-18 11:24:04 +0100

    design of query

--- src/pandoc/labs.py
+++ src/pandoc/labs.py
@@ -1,3 +1,56 @@
+"""
+Design
+================================================================================
+
+Fundamental object: Query.
+
+  - Query defined independently of the root(s) it will be applied to.
+
+  - Query is a callable ? Once defined, applied with `query(doc)` for
+    example ? Or stuff = query(doc) is the aim is some extraction ?
+    Or for item in query(doc): do_stuff_with(item). Root(s?) can be
+    a Pandoc item or a list or tuple of stuff, etc.
+    
+    Could also envision query methods directly on Pandoc Elements ...
+    dunno, have to think of it. Would be convenient (shorter code), 
+    but messy (Elements are not "algebraic datatypes" anymore).
+
+  - Query construction uses chaining (fluent) interface : chaining of
+    transforms, chaining of filters, etc.
+
+  - Query definition is lazy, does not compute anything: just abstract
+    description of what we intend to do.
+
+  - Query applied to a root : by default all items (in doc order)
+
+  - Query filters: may reduce the list of items. Chaining is an "AND" thus
+    a single-filter should be likely "OR"-based. E.g.
+    query.filter((Para, Plain)) means keep Para OR Plain.
+    Possible filters : filter(types), filter(predicate), has_attr(...),
+    id_is(id=...), etc. Need to have stuff to match the existence of a feature
+    has well as its value (ex: has id vs id is "main"). Also, class matching,
+    etc. Essentially, many syntaxic sugar on top of "filter(predicate)".
+    May need a meta stuff: .either(pred1, pred2, ...) ? Have a look at
+    shader graph to see how stuff is forked/join with such interfaces.
+
+  - Navigation: .parent, .children, .next_sibling, get_item[i]. Extract a 
+    general "navigate" function such that all that stuff is merely syntaxic
+    sugar ?
+
+  - Mutation: .remove(), .replace_with(stuff), etc. Probably complex to get it
+    right here. All this stuff should be TERMINAL : if any, nothing can be
+    chained after that. Also: set attributes, ids, etc.
+    Again, can we come up with a single, root, general mutation operation ?
+    Or is it too complex here?
+    Apply mutation in reverse order by default?
+
+  - Iteration ? Query as a container "removes" path info by default so that
+    we can deal with a list of "document items", make comprehensions, etc.
+
+
+"""
+
+
 import pandoc
 import pandoc.types
 
